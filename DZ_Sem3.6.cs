2e0b06922bd355f8d809359d5331a9a95a79eff4
Rick Solo <mrfox7398@mail.ru>
2022-09-08 11:19:41 +0300

    Zadacha 1

--- DZ_Sem3.6.cs
+++ DZ_Sem3.6.cs
@@ -5,8 +5,18 @@
 // 1, -7, 567, 89, 223-> 3
 
 Console.Write("Введите количество чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа: ");
+int[] array = new int[size];
+int count = 0;
 
-
+for (int i = 0; i < size; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i] > 0) count++;
+}
+Console.Write("[{0}]", string.Join(",", array));
+Console.WriteLine($" -> {count}");
 
 
 // ----------------------------------------------------------------------------------
